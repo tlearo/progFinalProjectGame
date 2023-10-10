12d5417b0c76645b2e23605b517e683b5a024cc2
t <33569285+tlearo@users.noreply.github.com>
2023-10-10 15:10:34 +1100

    create NPC.java class

---
+++ src/NPC.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class NPC {
+    public static void main(String[] args) {
+        //Tamara, Monica and Millie NPC.java
+        Scanner myInput = new Scanner(System.in);
+        String nl = "\n";
+    }
+}
