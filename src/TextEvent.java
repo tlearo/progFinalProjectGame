a1f8c6285223ee226104bd54b57175be0985f174
t <33569285+tlearo@users.noreply.github.com>
2023-10-10 15:11:43 +1100

    create TextEvent.java class

---
+++ src/TextEvent.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class TextEvent {
+    public static void main(String[] args) {
+        //Tamara, Monica and Millie TextEvent.java
+        Scanner myInput = new Scanner(System.in);
+        String nl = "\n";
+    }
+}
