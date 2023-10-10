c709b864952d25cca1d68540c169b8b92173f6b4
t <33569285+tlearo@users.noreply.github.com>
2023-10-10 15:08:08 +1100

    create Event.java class

---
+++ src/Event.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Event{
+    public static void main(String[] args) {
+        //Tamara, Monica and Millie Event.java
+        Scanner myInput = new Scanner(System.in);
+        String nl = "\n";
+    }
+}
