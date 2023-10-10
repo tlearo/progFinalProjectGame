cab9e8b187914ff170f5053e46d8b3dbbceb9ac6
t <33569285+tlearo@users.noreply.github.com>
2023-10-10 15:09:17 +1100

    create Inventory.java class

---
+++ src/Inventory.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+    public class Inventory {
+        public static void main(String[] args) {
+            //Tamara, Monica and Millie Inventory.java
+            Scanner myInput = new Scanner(System.in);
+            String nl = "\n";
+        }
+    }
