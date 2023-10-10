26482962e26daec6ad8a309002472f7b9f341d33
t <33569285+tlearo@users.noreply.github.com>
2023-10-10 15:05:57 +1100

    create Player.java class

---
+++ src/Player.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Player{
+    public static void main(String[] args) {
+        //Tamara, Monica and Millie player.java
+        Scanner myInput = new Scanner(System.in);
+        String nl = "\n";
+    }
+}
