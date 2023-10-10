c52b5eb9f0ff30ec81acd889a7bb996b7d2f2260
t <33569285+tlearo@users.noreply.github.com>
2023-10-10 15:07:07 +1100

    create Location.java class

---
+++ src/Location.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Location{
+    public static void main(String[] args) {
+        //Tamara, Monica and Millie Location.java
+        Scanner myInput = new Scanner(System.in);
+        String nl = "\n";
+    }
+}
