d97c35bc776792255b68efab772dc48350b7da85
t <33569285+tlearo@users.noreply.github.com>
2023-10-16 14:32:27 +1100

    test of inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,8 +1,49 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
     public class Inventory {
         public static void main(String[] args) {
             //Tamara, Monica and Millie Inventory.java
             Scanner myInput = new Scanner(System.in);
             String nl = "\n";
         }
+            private List<Item> items;
+            private int maxSize = 10;
+
+            public Inventory() {
+                this.items = new ArrayList<>();
+            }
+
+            public boolean addItem(String name, String description) {
+                    Item item = new Item(name, description);
+                    items.add(item);
+                    return true;
+            }
+
+            public boolean removeItem(Item item) {
+                return items.remove(item);
+            }
+
+            public List<Item> getItems() {
+                return items;
+            }
+
+            public class Item {
+                private String name;
+                private String description;
+
+                public Item(String name, String description) {
+                    this.name = name;
+                    this.description = description;
+                }
+
+                public String getName() {
+                    return name;
+                }
+
+                public String getDescription() {
+                    return description;
+                }
+            }
+
     }
