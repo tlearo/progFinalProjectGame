6a34940f75c5f6b5823f43775d1e59478db4578d
millieparker <139300989+millieparker@users.noreply.github.com>
2023-10-17 11:28:56 +1100

    - Added rest of the event squares with default methods to test them

--- src/Event.java
+++ src/Event.java
@@ -15,7 +15,42 @@ public class Event {
             case 'Q':
                 handleQuickSand(player);
                 break;
-
+            case 'B':
+                handleGetSword(player);
+                break;
+            case '~':
+                handleRiver(player);
+                break;
+            case 'C':
+                handleCottage(player);
+                break;
+            case 'S':
+                handleSpikeTrap(player);
+                break;
+            case '%':
+                handleForest(player);
+                break;
+            case 'X':
+                handleStartLocation(player);
+                break;
+            case '^':
+                handleMountain(player);
+                break;
+            case '$':
+                handleShop(player);
+                break;
+            case '@':
+                handleSuitor(player);
+                break;
+            case '!':
+                handleOgre(player);
+                break;
+            case 'E':
+                handleCavern(player);
+                break;
+            case 'D':
+                handleDragon(player);
+                break;
             default:
                 handleDefaultEvent(player);
                 break;
@@ -23,13 +58,45 @@ public class Event {
     }
 
     private void handleFairyCircle(Player player) {
-        System.out.print("testing Fairy circle");
+        System.out.println("testing Fairy circle");
     }
-
+    private void handleGetSword(Player player) {
+        System.out.println("testing sword");
+    }
+    private void handleRiver(Player player) {
+        System.out.println("testing river");
+    }
+    private void handleCottage(Player player) {
+        System.out.println("testing cottage");
+    }
+    private void handleSpikeTrap(Player player) {
+        System.out.println("testing spikes");
+    }
+    private void handleForest(Player player) {
+        System.out.println("testing forest"); }
+    private void handleStartLocation(Player player) {
+        System.out.println("testing start location");
+    }
+    private void handleMountain(Player player) {
+        System.out.println("testing mountain");
+    }
+    private void handleShop(Player player) {
+        System.out.println("testing shop");
+    }
+    private void handleSuitor(Player player) {
+        System.out.println("testing suitor");
+    }
+    private void handleOgre(Player player) {
+        System.out.println("testing ogre");
+    }
+    private void handleCavern(Player player) {
+        System.out.println("testing cavern");
+    }
+    private void handleDragon(Player player) {
+        System.out.println("testing dragon");
+    }
     private void handleQuickSand(Player player) {
-        // Implement your logic here
-    }
-
+        System.out.println("testing quicksand"); }
     private void handleDefaultEvent(Player player) {
         System.out.println("You've encountered an unknown area.");
     }
