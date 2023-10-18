cfbcbf4f844324ec0a1527c042199315a70a256f
millieparker <139300989+millieparker@users.noreply.github.com>
2023-10-18 23:10:53 +1100

    Changes to shop event

--- src/NPC.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class NPC {
-    public static void main(String[] args) {
-        //Tamara, Monica and Millie NPC.java
-        Scanner myInput = new Scanner(System.in);
-        String nl = "\n";
-    }
-}
