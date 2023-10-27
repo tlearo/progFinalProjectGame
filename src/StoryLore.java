3cc65eeaf87a0f0168796c3bf68da6ac1ea46ed0
t <33569285+tlearo@users.noreply.github.com>
2023-10-27 20:51:58 +1100

    adding back comments

--- src/StoryLore.java
+++ src/StoryLore.java
@@ -11,26 +11,26 @@ public class StoryLore {
     public static void Introduction() throws InterruptedException {
         Main.clearScreen();
         System.out.println("BEGINNING");
-        Separator(5);
+        Separator(5);//prints 5 lines
         System.out.println("As you walk through the forest, you see a poster claiming that a dragon is in need of help");
-        Thread.sleep(2000);
+        Thread.sleep(2000);//wait
         System.out.println("You look closer and that dragon looks familiar... Its your get-away bride!");
-        Thread.sleep(2000);
+        Thread.sleep(2000);//wait
         System.out.println("As you read the poster further, you realise your get-away bride is stuck on top of a mountain just south-east from where you are");
-        Thread.sleep(2000);
+        Thread.sleep(2000);//wait
         System.out.println("You want to help them out, but you also want to win their heart back; so you begin to think of ways to win them back...");
-        Thread.sleep(2000);
+        Thread.sleep(2000);//wait
         System.out.println("...");
-        Thread.sleep(1000);
+        Thread.sleep(1000);//wait
         System.out.println("...");
-        Thread.sleep(1000);
+        Thread.sleep(1000);//wait
         System.out.println("!!!");
-        Thread.sleep(2000);
+        Thread.sleep(2000);//wait
         System.out.println("You make plan to get them flowers, a gem, and a book as they loved those when you guys were together");
-        Thread.sleep(3000);
+        Thread.sleep(3000);//wait
         System.out.println("You are set on an adventure, shall we start?");
-        Thread.sleep(3000);
-        Separator(5);
+        Thread.sleep(3000);//wait
+        Separator(5);//prints 5 lines
     }
 }
 
