aa0373e0d0a167ac4634893f693eabedd31e05b6
t <33569285+tlearo@users.noreply.github.com>
2023-10-27 21:16:21 +1100

    comments

--- src/Inventory.java
+++ src/Inventory.java
@@ -3,63 +3,71 @@ import java.util.List;
 
 public class Inventory {
     public class Item {
-        private String name;
-        private String description;
-        private int cost;
-        private int hp;
+        private String name; // The name of the item.
+        private String description; // A description of the item.
+        private int cost;  // The cost or value of the item.
+        private int hp;  // Hit points (health) the item can restore (if applicable).
+
+        // Constructor for an item with a cost and hp.
         public Item(String name, String description, int cost,int hp) {
             this.name = name;
             this.description = description;
             this.cost = cost;
             this.hp = hp;
         }
+
+        // Constructor for an item without a cost or hp.
         public Item(String name, String description) {
             this.name = name;
             this.description = description;
         }
 
         public int getHp() {
-            return hp;
+            return hp; // Get the item's hit points value.
         }
 
         public String getName() {
-            return name;
+            return name;// Get the item's name.
         }
 
         public String getDescription() {
-            return description;
+            return description;// Get the item's description.
         }
 
         public int getCost() {
-            return cost;
+            return cost;// Get the item's cost or value.
         }
     }
 
-    private List<Item> items;
-    private int maxSize = 10;
+    private List<Item> items;// A list to store items in the inventory.
+    private int maxSize = 10;// The maximum number of items the inventory can hold.
 
     public Inventory() {
-        this.items = new ArrayList<>();
+        this.items = new ArrayList<>();// Initialize the inventory with an empty list of items.
     }
 
+    // Method to add an item with a cost and hp to the inventory.
     public boolean addItem(String name, String description, int cost, int hp) {
-        Item item = new Item(name, description, cost, hp);
-        items.add(item);
-        return true;
+        Item item = new Item(name, description, cost, hp);// Create a new item.
+        items.add(item);// Add the item to the inventory.
+        return true;// Return true to indicate successful addition.
     }
 
+    // Method to add an item without a cost or hp to the inventory.
     public boolean addItem(String name, String description) {
-        Item item = new Item(name, description);
-        items.add(item);
-        return true;
+        Item item = new Item(name, description);// Create a new item.
+        items.add(item);// Add the item to the inventory.
+        return true;// Return true to indicate successful addition.
     }
 
+    // Method to remove an item from the inventory.
     public boolean removeItem(Item item) {
-        return items.remove(item);
+        return items.remove(item);// Remove the specified item and return true if successful.
     }
 
+    // Method to get a list of all items in the inventory.
     public List<Item> getItems() {
-        return items;
+        return items;// Return the list of items in the inventory.
     }
 }
 
